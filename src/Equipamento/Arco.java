4bca25f263475062d3ad358d263b5a6d233b21b0
NASA <daniel_arod@protonmail.com>
2019-04-14 23:23:11 -0500

    añadido elfo oscuro  con su arma y armadura

---
+++ src/Equipamento/Arco.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Equipamento;
+
+/**
+ *
+ * @author nasa
+ */
+public class Arco implements Arma{
+
+    @Override
+    public void generarArma() {
+        //poner imagen y texto
+    }
+    
+}
---
+++ src/Equipamento/EscamasDragon.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Equipamento;
+
+/**
+ *
+ * @author nasa
+ */
+public class EscamasDragon implements Armadura {
+
+    @Override
+    public void generarArmadura() {
+        throw new UnsupportedOperationException("poner las escamas de dragon."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
